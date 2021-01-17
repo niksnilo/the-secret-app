56bc2c8e4b5c2ae8d05a10df12be2f2b35b6859e
niksnilo <businessniks26@gmail.com>
2021-01-17 22:51:30 +0800

    Add Page 404

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ if (port == null || port == "") {
 
 //404 Page not found
 app.use(function (req, res, next) {
-  res.status(404).send("Nothing Found");
+  res.status(404).render("404");
 });
 
 app.listen(port, function() {
