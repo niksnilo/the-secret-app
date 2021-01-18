a79a9f87cdf18dbb50221e560d38156d175ad900
niksnilo <businessniks26@gmail.com>
2021-01-18 12:07:02 +0800

    Hashing Passwords

--- README.md
+++ README.md
@@ -5,8 +5,7 @@ This app is sharing secrets with others anonymously.
 ## How to use it
 
 [Create an account](https://the-secret-app.herokuapp.com/register) to be able to see other's secrets.
-[Submit your secret] 
 
 ## License
 
-MIT
+[MIT License](https://github.com/niksnilo/the-secret-app/blob/main/LICENSE)
--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -25,8 +25,6 @@ const userSchema = new mongoose.Schema({
     }
 });
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
-
 //Table Name
 const User = new mongoose.model("User", userSchema);
 
@@ -41,7 +39,7 @@ app.route("/login")
     })
     .post(function(req, res){
         const username = req.body.username;
-        const password = req.body.password;
+        const password = md5(req.body.password);
 
         User.findOne({email: username}, function(err, foundUser){
             if(foundUser && !err){
@@ -62,7 +60,7 @@ app.route("/register")
     .post(function(req, res){
         const newUser = new User({
             email: req.body.username,
-            password: req.body.password
+            password: md5(req.body.password)
         });
 
         newUser.save(function(err){
