95570956e379fe14930ff59c042e7cef144cf1c7
niksnilo <businessniks26@gmail.com>
2021-01-19 14:38:15 +0800

    Fix OAuth

--- app.js
+++ app.js
@@ -60,6 +60,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
+    // callbackURL: "http://localhost:3000/auth/google/secrets",
     callbackURL: "https://the-secret-app.herokuapp.com/auth/google/secrets",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
