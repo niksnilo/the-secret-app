72d36749a0d8ca91d5471db3acfe6ee2c77df415
niksnilo <businessniks26@gmail.com>
2021-01-19 19:40:26 +0800

    Change Callback URL

--- app.js
+++ app.js
@@ -63,8 +63,8 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets",
-    // callbackURL: "https://the-secret-app.herokuapp.com/auth/google/secrets",
+    // callbackURL: "http://localhost:3000/auth/google/secrets",
+    callbackURL: "https://the-secret-app.herokuapp.com/auth/google/secrets",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
